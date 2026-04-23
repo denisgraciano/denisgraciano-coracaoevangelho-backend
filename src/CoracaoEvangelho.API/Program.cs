9cd391619c5262c468cfd60e0eb9f1b5e7e16f12
denisgraciano <denisgraciano@gmail.com>
2026-04-23 11:48:15 -0300

    ajuste para prod

--- src/CoracaoEvangelho.API/Program.cs
+++ src/CoracaoEvangelho.API/Program.cs
@@ -213,7 +213,10 @@ using (var scope = app.Services.CreateScope())
 Log.Information("Coração Evangelho API iniciada. Ambiente: {Env}", app.Environment.EnvironmentName);
 Log.Information("Frontend configurado: {Url}", frontendUrl);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
+if (!app.Environment.IsDevelopment())
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+    app.Urls.Add($"http://*:{port}");
+}
 
 app.Run();
